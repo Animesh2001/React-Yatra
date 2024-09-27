352ed15725439bda3a7b7ae6d48c86ce7da8838d
Animesh2001 <animesh2jaiswal@gmail.com>
2024-09-27 12:13:26 +0530

    using react via CDN

---
+++ index.js
@@ -0,0 +1,12 @@
+// this is how react also use to work.
+// const root = document.getElementById('root')
+// const p = document.createElement('p')
+// p.innerText = "Hello from Animesh part 2";
+// root.appendChild(p)
+
+const child1 = React.createElement('p',{},"I am a child")
+const child2 = React.createElement('p',{},"I am a child")
+const div = React.createElement('div',{className:'text'},[child1,child2])
+console.log(div)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(div);  
