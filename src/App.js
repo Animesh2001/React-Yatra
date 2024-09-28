3f00ca3b85c4e31ec9547f60ecd01140970a430a
Animesh2001 <animesh2jaiswal@gmail.com>
2024-09-28 18:58:00 +0530

    Render data from array , Importance of keys in maps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Button from './Button';
+import Text from './text'
 
 const App = () => {
-    //we want to render this button component
-    const handleClickAction = ()=>{
-        console.log('I was clicked from parent.')
-    }
-    return <Button clickAction = {handleClickAction}>Click Me</Button> 
+    //now we will render something using this data
+    const data = [
+        { id: 'a', text: "text 1" },
+        { id: 'b', text: "text 2" },
+        { id: 'c', text: "text 3" },
+        { id: 'd', text: "text 4" },
+        { id: 'e', text: "text 5" }
+    ]
+
+    //we can do like this , but instead of doing this we can do something better.
+    // return <>
+    //     <Text>{data[0].text}</Text>
+    //     <Text>{data[1].text}</Text>
+    //     <Text>{data[2].text}</Text>
+    //     <Text>{data[3].text}</Text>
+    //     <Text>{data[4].text}</Text>
+
+    // </>
+
+    //using map we can generate the array dynamically in javascript.
+    return <>
+        {data.map((item,index)=> <Text key={index}>{item.text}</Text>)}   
+</>
+
 }
 
 export default App;
---
+++ src/text.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const Text = (props) =>{
+    const {children} = props
+    return <div>{children}</div>
+}
+
+export default Text;
